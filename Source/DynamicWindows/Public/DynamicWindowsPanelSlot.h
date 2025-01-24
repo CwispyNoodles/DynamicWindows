b4a65c41b4a51425d652be466fa29e7f7d420f1e
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-24 13:56:52 -0500

    Created DynamicWindowsPanelSlot

---
+++ Source/DynamicWindows/Public/DynamicWindowsPanelSlot.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CanvasPanelSlot.h"
+#include "DynamicWindowsPanelSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICWINDOWS_API UDynamicWindowsPanelSlot : public UCanvasPanelSlot
+{
+	GENERATED_BODY()
+};
