8f8b28edc42cff4230e7b3baf1f610453fb28527
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-01-24 13:56:10 -0500

    Created DynamicWindowsPanel

---
+++ Source/DynamicWindows/Public/DynamicWindowsPanel.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CanvasPanel.h"
+#include "DynamicWindowsPanel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICWINDOWS_API UDynamicWindowsPanel : public UCanvasPanel
+{
+	GENERATED_BODY()
+};
